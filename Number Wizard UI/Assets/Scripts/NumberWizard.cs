f5e7591c2fd8cf7a5cd3b6e5b3ae56136fc4ef84
Joshua Boensch <josh.boensch@gmail.com>
2018-11-01 13:58:10 -0400

    Section 4 - Lecture 41

--- Number Wizard UI/Assets/Scripts/NumberWizard.cs
+++ Number Wizard UI/Assets/Scripts/NumberWizard.cs
@@ -1,11 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 public class NumberWizard : MonoBehaviour {
 
     [SerializeField] int max;
     [SerializeField] int min;
+    [SerializeField] TextMeshProUGUI guessText;
     int guess;
 
     // Use this for initialization
@@ -16,20 +17,23 @@ public class NumberWizard : MonoBehaviour {
 	
     void StartGame()
     {
-        max = max + 1;
         NextGuess();
+        guessText.text = guess.ToString();
+        max = max + 1;
     }
 
     public void OnPressHigher()
     {
         min = guess;
         NextGuess();
+        guessText.text = guess.ToString();
     }
 
     public void OnPressLower()
     {
         max = guess;
         NextGuess();
+        guessText.text = guess.ToString();
     }
     
     void NextGuess()
